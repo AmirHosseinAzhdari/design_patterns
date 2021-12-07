4219f8ba781959a5d7334691f3609fe1558baf35
Ah_azhdari <dev.amirhossein.azhdari@outlook.com>
2021-12-07 15:16:16 +0330

    delete two files

--- main.py
+++
@@ -1,9 +0,0 @@
-"""
-    Design patterns:
-        1 - Creational
-        2 - Structural -> communicate between microservices
-        3 - Behavioral
-        4 - Concurrency
-        
-    * 22 design patterns.
-"""
